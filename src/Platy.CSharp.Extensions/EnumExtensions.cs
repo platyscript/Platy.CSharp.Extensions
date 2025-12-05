83c81049787b94f7a372b0a99142de918a809de7
platyscript <244315239+platyscript@users.noreply.github.com>
2025-12-05 11:48:07 +0530

    Added Tests

---
+++ src/Platy.CSharp.Extensions/EnumExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+
+namespace Platy.CSharp.Extensions;
+
+public static class EnumExtensions
+{
+  
+  // Cache to store display names of enum values.
+  private static readonly ConcurrentDictionary<Enum, string> DisplayNameCache = new();
+
+
+  /// <summary>
+  ///     Gets an attribute on an enum field value.
+  /// </summary>
+  /// <param name="enumValue">The enum value</param>
+  /// <typeparam name="T">The type of the attribute to retrieve.</typeparam>
+  /// <returns>The attribute of the specified type or null</returns>
+  public static T? GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
+  {
+    var type = enumValue.GetType();
+
+    // Use GetField to field info for the enum value
+    var memInfo = type.GetField(enumValue.ToString(), BindingFlags.Public | BindingFlags.Static);
+
+    if (memInfo == null)
+    {
+      return null;
+    }
+
+    // Retrieve the custom attributes of type T
+    var attributes = memInfo.GetCustomAttributes<T>(false);
+    return attributes.FirstOrDefault();
+  }
+
+  /// <summary>
+  ///     Gets the enum display name.
+  /// </summary>
+  /// <param name="enumValue">The enum value.</param>
+  /// <returns>
+  ///     Use <see cref="DisplayAttribute" /> if it exists.
+  ///     Otherwise, use the standard string representation.
+  /// </returns>
+  public static string GetDisplayName(this Enum enumValue)
+  {
+    // Retrieve the display name from the cache if it exists
+    return DisplayNameCache.GetOrAdd(enumValue, e =>
+    {
+      // Get the DisplayAttribute
+      var attribute = e.GetAttributeOfType<DisplayAttribute>();
+
+      // Return the DisplayAttribute name if it exists, otherwise return the enum's string representation
+      return attribute?.Name is not null ? attribute.Name : e.ToString();
+    });
+  }}
---
+++ test/Platy.CSharp.Extensions.Tests/Attributes/DisplayAttributeTests.cs
@@ -0,0 +1,52 @@
+﻿using Platy.CSharp.Extensions;
+
+namespace Platy.CSharp.Extensions.Tests.Attributes;
+
+public enum ApiLevel
+{
+  [DisplayAttribute("private")] Private = 1,
+  [DisplayAttribute("public")] Public = 2,
+  [DisplayAttribute("corporate")] Corporate = 3
+}
+
+[Flags]
+public enum UserType
+{
+  [DisplayAttribute("admin")] Admin = 1,
+  [DisplayAttribute("editor")] Editor = 2,
+  [DisplayAttribute("publisher")] Publisher = 3,
+  [DisplayAttribute("all")] All = Admin | Editor | Publisher
+}
+public class DisplayAttributeTests
+{
+  
+  [Theory]
+  [InlineData(ApiLevel.Private, "private")]
+  [InlineData(ApiLevel.Public, "public")]
+  [InlineData(ApiLevel.Corporate, "corporate")]
+  public void GetDisplayNameExtensionShouldUseDisplayAttribute(ApiLevel apiLevel, string expected)
+  {
+    Assert.Equal(expected, apiLevel.GetDisplayName());
+  }
+
+  [Theory]
+  [InlineData(ApiLevel.Private, "private")]
+  [InlineData(ApiLevel.Public, "public")]
+  [InlineData(ApiLevel.Corporate, "corporate")]
+  public void GetEnumFromDisplayNameShouldReturnEnumValue(ApiLevel expected, string displayName)
+  {
+    displayName.TryGetEnumFromDisplayName<ApiLevel>(out var result);
+    Assert.Equal(expected, result);
+  }
+    
+    
+  [Theory]
+  [InlineData(UserType.Admin,"admin")]
+  [InlineData(UserType.Publisher, "publisher")]
+  [InlineData(UserType.Editor, "editor")]
+  [InlineData(UserType.All, "all")]
+  public void GetEnumFromDisplayNameShouldReturnEnumValueForFlagsEnum(UserType expected, string displayName)
+  {
+    displayName.TryGetEnumFromDisplayName<UserType>(out var result);
+    Assert.Equal(expected, result);
+  }}
